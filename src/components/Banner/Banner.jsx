646360c004955587a466b481e86177b5664ddddd
Adityakitukale49 <adityakitukale4599@gmail.com>
2023-08-23 17:55:36 +0530

    ds

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -19,26 +19,28 @@ const Banner = () => {
     }
 
     return (
-        <header className="banner"
-            style={{
-                backgroundSize: "cover",
-                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-                backgroundPosition: "center center",
-            }}
-        >
-            <div className="banner-contents">
-                <h1 className="banner-title">
-                    {movie?.title || movie?.name || movie?.original_name}
-                </h1>
-                <div className="banner-buttons">
-                    <button className="banner-button">Play</button>
-                    <button className="banner-button">My List</button>
-                </div>
-                <h1 className="banner-description">{truncate(movie?.overview, 150)}</h1>
+        <>
+            <header className="banner"
+                style={{
+                    backgroundSize: "cover",
+                    backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+                    backgroundPosition: "center center",
+                }}
+            >
+                <div className="banner-contents">
+                    <h1 className="banner-title">
+                        {movie?.title || movie?.name || movie?.original_name}
+                    </h1>
+                    <div className="banner-buttons">
+                        <button className="banner-button">Play</button>
+                        <button className="banner-button">My List</button>
+                    </div>
+                    <h1 className="banner-description">{truncate(movie?.overview, 150)}</h1>
 
-            </div>
-            <div className="banner-fadeBottom"></div>
-        </header>
+                </div>
+                <div className="banner-fadeBottom"></div>
+            </header>
+        </>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import TabComponent from '../components/Tabs'
-import Login from './Login'
+// import Login from './Login'
 
 const Home = () => {
   return (
     <>
-      <Login />
+      {/* <Login /> */}
       <TabComponent />
     </>
   )
